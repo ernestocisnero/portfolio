2fc393fcf582e828c2c94319162a9c278e733f90
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2026-01-24 22:16:36 +0100

    Remove text below Name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ export default function Header() {
   return (
     <header>
       <h1 className="name">Ernesto Cisnero</h1>
-      <p className="title">Automation Engineer / Developer by Hobby</p>
       <p className="intro">
         I'm an automation and electrical engineer who enjoys writing code for fun — just building things and learning new technologies along the way.
       </p>
