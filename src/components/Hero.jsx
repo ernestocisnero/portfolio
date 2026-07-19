0144b102cb97f935c36643b7125e541a4d0a096c
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2026-07-18 23:30:44 -0400

    Portfolio refactor

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <h1>
+        Building simple,
+        <br />
+        useful apps.
+      </h1>
+
+      <p>
+        I'm Ernesto Cisnero, an independent developer creating
+        software experiences for iOS.
+      </p>
+
+      <div className="hero-links">
+        <a href="/apps/launchtrack/">LaunchTrack</a>
+      </div>
+    </section>
+  );
+}
